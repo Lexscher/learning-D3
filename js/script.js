e8735983a41504c1ee095d394f1167a061ca27b8
Alexander Schelchere <ascellie@gmail.com>
2018-06-26 12:19:46 -0400

    Lecture 11: complete

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ console.log("You're awesome!");
 let svg = d3
   .select("#chart-area")
   .append("svg")
-  .attr("width", 400)
+  .attr("width", 500)
   .attr("height", 400);
 
 let circle = svg
@@ -20,3 +20,34 @@ let rectangle = svg
   .attr("width", 200)
   .attr("height", 100)
   .attr("fill", "#ffe4e1");
+
+let text = svg
+  .append("text")
+  .attr("x", 250)
+  .attr("y", 200)
+  .attr("font-size", 20)
+  .attr("font-family", "IM Fell English SC, serif")
+  .attr("fill", "#fce9f0");
+
+let line = svg
+  .append("line")
+  .attr("x1", 175)
+  .attr("y1", 230)
+  .attr("x2", 230)
+  .attr("y2", 40)
+  .attr("stroke", "#66ccff")
+  .attr("stroke-width", 10);
+
+let ellipese = svg
+  .append("ellipse")
+  .attr("cx", 315)
+  .attr("cy", 330)
+  .attr("rx", 130)
+  .attr("ry", 10)
+  .attr("fill", "#66ccff");
+
+let path = svg
+  .append("path")
+  .attr("d", "M20 80 C 140 10, 95 10, 95 80 S 150 50, 180 80")
+  .attr("stroke", "#fff")
+  .attr("fill", "transparent");
